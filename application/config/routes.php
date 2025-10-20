0bc4d88153fa521a9de12175b18d111eac2e0826
Razi <shirazinur328@gmail.com>
2025-10-20 08:43:16 +0700

    ini configurasi awal CI 3

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'admin';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['admin'] = 'admin/mapel';
+$route['admin/(:any)'] = 'admin/$1';
+
+// murid routes
+$route['murid'] = 'murid/dashboard';
+$route['murid/(:any)'] = 'murid/$1';
